60fd5eefb875c70314b63dee559808b39453f01e
Michael L. Book <mikebook24@gmail.com>
2022-07-12 23:44:14 +0000

    updated

--- src/js/component/task.jsx
+++ src/js/component/task.jsx
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
-import propTypes from "prop-types"
+
 const Task = (props) => {
     const[visible, toggleVisibility] = useState(false)
 
-
-
     return (
-        
         <li 
-        onMouseEnter={() => toggleVisibility(true)}
-        onMouseLeave={() => toggleVisibility(false)}
-        >{props.listItem}
+            onMouseEnter={() => toggleVisibility(true)}
+            onMouseLeave={() => toggleVisibility(false)}
+        >
+            {props.listItem}
             <button
-                className={(visible == true) ? "btn DelItem float-end text-danger" : "d-none"}
-                onClick={() => {props.removeItem(i); }}>
+                className={visible ? "btn DelItem float-end text-danger" : "d-none"}
+                onClick={() => props.removeItem(props.index)}
+            >
                 <i className="fa fa-trash p-2 flex-shrink-1" />
             </button>
         </li>
-    
     )
 }
 
