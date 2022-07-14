29181328581d01dba9528449fb239f92042c204c
TysonR0319 <tysonr0319@gmail.com>
2022-07-14 22:13:32 +0000

    fetch introduced

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,89 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TaskInput from "./taskInput.jsx"
 import Task from "./task.jsx";
 
 
 const Home = () => {
 	const [list, setList] = useState([])
-	
-	const addItem = (li) => {
-		const newList = [...list, li]
-		setList(newList)
+	//UseEffect
+	useEffect(() => {
+		fetchListItem()
+	}, [])
+
+
+	//ApiCallFunction
+	const fetchListItem = () => {
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
+			method: "Get",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {		//Getting Status on fetch
+				if (resp.status == 200) {
+					return resp.json();
+				}
+			})
+			.then((data) => {	//Sending data to useState(list items)
+				setList(data);
+				console.log(data);
+			})
+			.catch((error) => {	//catching errors
+				console.log(error);
+			})
+
 	}
 
+
+
+	//AddItem function
+	const addItem = (li) => {
+		const newTodo = {label:li, done:false}
+		const newList = [...list, newTodo]
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
+			method: "PUT",
+			body: JSON.stringify(newList),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+		.then(resp => {		//Getting Status on fetch
+			if (resp.status == 200) {
+				return resp.json();
+			}
+		})
+		.then((data) => {	
+			alert(data.result)
+			fetchListItem()
+		})
+		.catch((error) => {	//catching errors
+			console.log(error);
+		})
+
+	}
+	//removeItem
 	const removeItem = (index) => {
-		const newToDos = [...list].filter((item, i) => i != index)
-		setList(newToDos)
+		const newList = [...list]
+		newList[index].done = true
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
+			method: "PUT",
+			body: JSON.stringify(newList),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+		.then(resp => {		//Getting Status on fetch
+			if (resp.status == 200) {
+				return resp.json();
+			}
+		})
+		.then((data) => {	
+			alert(data.result)
+			fetchListItem()
+		})
+		.catch((error) => {	//catching errors
+			console.log(error);
+		})
 	}
 
 	return (
@@ -21,7 +91,7 @@ const Home = () => {
 			<h1>To-Do</h1>
 			<TaskInput addItem={addItem} />
 			{list.map((li, i) => {
-				return <Task key={`${li}-${i}`} listItem={li} index={i} removeItem={removeItem} />
+				return <Task key={`${li.label}-${i}`} listItem={li.label} index={i} removeItem={removeItem} />
 			})}
 			{list.length}
 		</div>
@@ -29,5 +99,3 @@ const Home = () => {
 };
 
 export default Home;
-
-
